6bffdb0e1a68634f14fd627a80acdda607aa8986
Al Urim <almostalbert@gmail.com>
2018-12-02 21:39:46 -0800

    Day 1

---
+++ day-1/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+const readline = require("readline");
+const stream = require("stream");
+
+// Code stolen from https://medium.com/@wietsevenema/node-js-using-for-await-to-read-lines-from-a-file-ead1f4dd8c6f
+function readLines({ input }) {
+  const output = new stream.PassThrough({ objectMode: true });
+  const rl = readline.createInterface({ input });
+  rl.on("line", line => {
+    output.write(line);
+  });
+  rl.on("close", () => {
+    output.push(null);
+  });
+  return output;
+}
+
+const input = fs.createReadStream(path.resolve(__dirname, "input"));
+(async () => {
+  let counter = 0;
+  for await (const line of readLines({ input })) {
+    const lineNumber = parseInt(line.substr(1), 10);
+    if (line[0] === "-") {
+      counter = counter - lineNumber;
+    } else if (line[0] === "+") {
+      counter = counter + lineNumber;
+    } else {
+      throw new Error("Unexpected line!", line);
+    }
+  }
+  console.log(counter);
+})();
